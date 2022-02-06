d3bee31ae525ebb8bb533dfefa47cd11c27fc28e
MaelleAlliou <93644904+MaelleAlliou@users.noreply.github.com>
2022-02-06 15:43:49 +0100

    Add files via upload

---
+++ GD1A_SIDE_ALLIOU_Maelle/SIDE SCROLLER/assets/tuiles_de_jeu.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="SS - TILESET 02" tilewidth="32" tileheight="32" tilecount="30" columns="10">
+ <image source="C:/Users/maell/OneDrive/Documents/ETPA/LD/SS - TILESET 02.png" width="320" height="96"/>
+</tileset>
